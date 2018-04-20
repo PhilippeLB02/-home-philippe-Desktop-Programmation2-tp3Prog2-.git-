8c7511148a87dea98843f26e31dad944f7d700f3
kismonyladouceur <kismony7@hotmail.com>
2018-04-19 20:01:42 -0400

    fenetre 3 bouton (suivant) fonctionnel la fenetre 3 recoit un test (plus un string)

--- src/FenetreFaireTests.java
+++ src/FenetreFaireTests.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class FenetreFaireTests {
 
@@ -25,9 +28,10 @@ public class FenetreFaireTests {
 
     //Enoncé
     private JLabel QUESTION;
-    private int numeroQuestion = 1;
+    //private int numeroQuestion = 1;
     private JLabel enoncer;
     private JTextArea question;
+
 
     //Reponse
     private JLabel reponse;
@@ -41,16 +45,22 @@ public class FenetreFaireTests {
     private JTextField reponseQuatre;
 
     //Boutons radio
-    private JRadioButton repBout1;
-    private JRadioButton repBout2;
-    private JRadioButton repBout3;
-    private JRadioButton repBout4;
+    private JCheckBox repBout1;
+    private JCheckBox repBout2;
+    private JCheckBox repBout3;
+    private JCheckBox repBout4;
 
-    public FenetreFaireTests(String nomTest){
-        init(nomTest);
+    //test
+    private Test testFenetre;
+    private String []choixReponseTest;
+
+    public FenetreFaireTests(Test test){
+        testFenetre = test;
+        choixReponseTest = new String[4];
+        init(testFenetre);
     }
 
-    private void init(String nomTest){
+    private void init(Test test){
 
         fenetreFaireTest = new JFrame("Faire un test");
         fenetreFaireTest.setBounds(400,300,LARGEUX_FENTRE,HAUTEUR_FENETRE);
@@ -67,36 +77,31 @@ public class FenetreFaireTests {
 
         nomDuTest.setBounds(fenetreFaireTest.getWidth() / 12 , fenetreFaireTest.getHeight() / 13 - HAUT_BTN, 200, HAUT_BTN);
         nomTests.setBounds(fenetreFaireTest.getWidth() / 12 * 3, fenetreFaireTest.getHeight() / 13 - HAUT_BTN, 350, HAUT_BTN);
-
+        nomTests.setText(testFenetre.getName());
         fenetreFaireTest.getContentPane().add(nomDuTest);
         fenetreFaireTest.getContentPane().add(nomTests);
 
 
         //Enoncer
-        QUESTION = new JLabel("Question " + numeroQuestion);
+        QUESTION = new JLabel("Question " + testFenetre.getQuestionCourante());
         QUESTION.setBounds(10, 10, 200, HAUT_BTN);
         enoncer = new JLabel("Énoncé ");
         enoncer.setBounds(25, 40, 200, HAUT_BTN);
         question = new JTextArea();
         question.setLineWrap(true);
-        question.setEditable(true);
+        question.setEditable(false);
         question.setBounds(25, 80, 400, HAUT_BTN * 2);
+        question.setText(testFenetre.getQuestion().get(testFenetre.getQuestionCourante()));
 
+        JScrollPane barreNavigation = new JScrollPane(question);
+
+        barreNavigation.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
         panneauDesQuestion = new JPanel();
         panneauDesQuestion.setBounds(50,50, 450, 350);
         panneauDesQuestion.setOpaque(true);
         panneauDesQuestion.setLayout(null);
 
-
-        QUESTION = new JLabel("Question " + numeroQuestion);
-        QUESTION.setBounds(10, 10, 200, HAUT_BTN);
-        enoncer = new JLabel("Énoncé ");
-        enoncer.setBounds(25, 40, 200, HAUT_BTN);
-        question = new JTextArea();
-        question.setLineWrap(true);
-        question.setEditable(true);
-        question.setBounds(25, 80, 400, HAUT_BTN * 2);
 
         panneauDesQuestion.add(QUESTION);
         panneauDesQuestion.add(enoncer);
@@ -121,14 +126,21 @@ public class FenetreFaireTests {
         reponseTrois.setBounds(70, 270, 310, HAUT_BTN);
         reponseQuatre = new JTextField(35);
         reponseQuatre.setBounds(70, 310, 310, HAUT_BTN);
-        repBout1 = new JRadioButton();
+        repBout1 = new JCheckBox();
         repBout1.setBounds(390, 190, LARG_BTN, HAUT_BTN);
-        repBout2 = new JRadioButton();
+        repBout2 = new JCheckBox();
         repBout2.setBounds(390, 230, LARG_BTN, HAUT_BTN);
-        repBout3 = new JRadioButton();
+        repBout3 = new JCheckBox();
         repBout3.setBounds(390, 270, LARG_BTN, HAUT_BTN);
-        repBout4 = new JRadioButton();
+        repBout4 = new JCheckBox();
         repBout4.setBounds(390, 310, LARG_BTN,HAUT_BTN);
+        ButtonGroup groupBouton = new ButtonGroup();
+        groupBouton.add(repBout1);
+        groupBouton.add(repBout2);
+        groupBouton.add(repBout3);
+        groupBouton.add(repBout4);
+
+        afficherQuestion(0);
 
         panneauDesQuestion.add(reponse);
         panneauDesQuestion.add(reponse1);
@@ -143,8 +155,6 @@ public class FenetreFaireTests {
         panneauDesQuestion.add(reponse4);
         panneauDesQuestion.add(reponseQuatre);
         panneauDesQuestion.add(repBout4);
-
-
 
 
         panneauDesQuestion.add(QUESTION);
@@ -169,11 +179,39 @@ public class FenetreFaireTests {
         boutonCorrection.setBounds(fenetreFaireTest.getWidth() / 2 - LARG_BTN_CORR / 2 - 4, fenetreFaireTest.getHeight() / 13 * 12 - HAUT_BTN,
                 LARG_BTN_CORR, HAUT_BTN);
 
+        boutonSuivant.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(repBout1.isSelected() || repBout2.isSelected() || repBout3.isSelected() || repBout4.isSelected()){
+
+                    if (testFenetre.getQuestionCourante() + 1 == testFenetre.getNbQuestion())
+                        System.out.println("nop");
+                    else
+                        testFenetre.setQuestionCourante(testFenetre.getQuestionCourante() + 1);
+
+                    afficherQuestion(testFenetre.getQuestionCourante());
+                }
+            }
+        });
+
+
         fenetreFaireTest.getContentPane().add(boutonPrecedent);
         fenetreFaireTest.getContentPane().add(boutonSuivant);
         fenetreFaireTest.getContentPane().add(boutonCorrection);
         fenetreFaireTest.add(panneauDesQuestion);
 
         fenetreFaireTest.setVisible(true);
+    }
+    private void afficherQuestion(int numeroQuestion){
+        //String[] chx = values.split("\\|",-1);
+        //System.out.println("question courante = "+testFenetre.getQuestionCourante());
+        QUESTION.setText("Question "+ (testFenetre.getQuestionCourante()+1));
+        question.setText(testFenetre.getQuestion(numeroQuestion));
+        choixReponseTest = testFenetre.getChoixReponse().get(numeroQuestion).split("<>");
+        reponseUn.setText(choixReponseTest[0]);
+        reponseDeux.setText(choixReponseTest[1]);
+        reponseTrois.setText(choixReponseTest[2]);
+        reponseQuatre.setText(choixReponseTest[3]);
+        //testFenetre.getChoixReponse().s
     }
 }
