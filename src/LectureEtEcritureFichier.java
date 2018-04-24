dd4387ccb277b5e75df61518032a06a77141b3c9
kismonyladouceur <kismony7@hotmail.com>
2018-04-24 16:50:30 -0400

    jai ajoute l'extension txt au chemin lorsqu'on ecrit et lit

--- src/LectureEtEcritureFichier.java
+++ src/LectureEtEcritureFichier.java
@@ -9,7 +9,7 @@ public class LectureEtEcritureFichier {
     private static final String SÉPARATEUR_QUESTIONS = "-----";
 
     //nom du fichier a utiliser
-    private static File SAUVEGARDETEST = new File(GenerateurTests.class.getProtectionDomain().getCodeSource().getLocation().getPath() + "tests");
+    private static File SAUVEGARDETEST = new File(GenerateurTests.class.getProtectionDomain().getCodeSource().getLocation().getPath() + "tests.txt");
 
     /**
      * getters pour aller chercher le nom du fichier
@@ -72,7 +72,7 @@ public class LectureEtEcritureFichier {
      */
     public static ArrayList<Test> lecture() throws IOException {
 
-        String tests = GenerateurTests.class.getProtectionDomain().getCodeSource().getLocation().getPath() + "tests";
+        String tests = GenerateurTests.class.getProtectionDomain().getCodeSource().getLocation().getPath() + "tests.txt";
         String line="";
         String tempStr=line;
 
